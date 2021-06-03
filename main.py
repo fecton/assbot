8dc093bdd1575289a0a78bcd15666c0d7c9f8015
a <blablasa@gmail.com>
2021-06-03 17:31:11 +0300

    something has been written

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ import random
 import sqlite3
 import os
 
+queries = {
+    "create_table" : """
+        CREATE TABLE `users`(
+                id          INTEGER     PRIMARY KEY NOT NULL,
+                username    TEXT                    NOT NULL,
+                name        TEXT                    NOT NULL,
+                length      INTEGER                 NOT NULL,
+                timeleft    INTEGER                 NOT NULL
+            );
+    """,
+    "put_user" : """
+        INSERT INTO `users`(id,username, name, length, timeleft)
+        VALUES (?,?,?,?,?)
+    """
+}
+
+
 from aiogram import Bot, Dispatcher, executor, types
 
 # database initialization
@@ -21,12 +38,7 @@ if "list" in os.listdir("."):
     print("[+] Database had already created!")
 else:
     db = sqlite3.connect("list")
-    db.execute("""
-        CREATE TABLE `users`(
-                username    TEXT        PRIMARY KEY NOT NULL,
-                name        TEXT        NOT NULL,
-                length      INTEGER     NOT NULL );
-    """)
+    db.execute(queries["create_table"])
     print("[+] Database is successfully created!")
     db.commit()
     db.close()
@@ -48,16 +60,54 @@ if config.DEBUG == True:
 # dialogs
 content = json.loads( open("dialogs.json","r",encoding="utf8").read())
 # ass script
-@dp.message_handler(commamds=["ass"])
+@dp.message_handler(commands=["ass"])
 async def ass(message: types.Message):
+    if message.chat["id"] == "495137368":
+        await message.answer("Бот не работает только в одной группе!")
+    else:
+        db = sqlite3.connect("list")
+        # if user exists in database
 
-    await message.answer(content["help"])
+        cursor = db.execute("""
+                SELECT * FROM `users` WHERE id={0}
+        """.format(message.from_user["id"]))
+
+        ass_info = cursor.fetchall()[0]
+        if ass_info == []:
+            userinfo = (message.from_user["id"], message.from_user["username"], message.from_user["first_name"], 0, 0)
+            db.execute(queries["put_user"], userinfo)
+            await message.answer("joined")
+        else:
+            # ass script
+            tmp_length = random.randint(-10,15)
+
+            if tmp_length == 0:
+                # message with no profit
+                pass
+            elif tmp_length > 0:
+                # message with profit
+                pass
+            elif tmp_length < 0:
+                # message with bad profit
+                pass
+            # ass_info[3]
+            ass_info = list(ass_info)
+            ass_info[3] = ass_info[3]+tmp_length
+            userinfo = tuple(ass_info)
+
+            await message.answer(userinfo)
+
+        db.commit()
+        db.close()
+        await message.answer("complete")
+
 # help
-@dp.message_handler(commamds=["help"])
+@dp.message_handler(commands=["help"])
 async def help(message: types.Message):
     await message.answer(content["help"])
+
 # menu
-@dp.message_handler(commamds=["menu"])
+@dp.message_handler(commands=["menu"])
 async def menu(message: types.Message):
     await message.answer("Тут должно быть меню")
 
