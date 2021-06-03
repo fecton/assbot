7bd4f115ce8df07ac6151601b65a8001e205d4c5
a <blablasa@gmail.com>
2021-06-03 22:17:26 +0300

    i didn't remember what i was writing here

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 
 # TODO написать основной скрипт для функции "ass"
 # TODO оформить базу данных
-# TODO сделать чтение и сравнивание данных из базы данных
-# TODO фиксиить ошибки
 # TODO написать диалоги
+# TODO написать нормально отчёты и сравнивание за временем
 
 import config
 import json
 import random
 import sqlite3
 import os
+import time
 
 queries = {
     "create_table" : """
@@ -20,12 +20,13 @@ queries = {
                 username    TEXT                    NOT NULL,
                 name        TEXT                    NOT NULL,
                 length      INTEGER                 NOT NULL,
-                timeleft    INTEGER                 NOT NULL
+                starttime   INTEGER                 NOT NULL,
+                endtime     INTEGER                 NOT NULL
             );
     """,
     "put_user" : """
-        INSERT INTO `users`(id,username, name, length, timeleft)
-        VALUES (?,?,?,?,?)
+        INSERT INTO `users`(id,username, name, length, starttime, endtime)
+        VALUES (?,?,?,?,?,?)
     """
 }
 
@@ -63,53 +64,91 @@ content = json.loads( open("dialogs.json","r",encoding="utf8").read())
 @dp.message_handler(commands=["ass"])
 async def ass(message: types.Message):
     if message.chat["id"] == "495137368":
-        await message.answer("Бот не работает только в одной группе!")
+        await message.answer("Бот работает только в одной группе!")
     else:
         db = sqlite3.connect("list")
         # if user exists in database
 
         cursor = db.execute("""
-                SELECT * FROM `users` WHERE id={0}
+        SELECT * FROM `users` WHERE id={0}
         """.format(message.from_user["id"]))
-
-        ass_info = cursor.fetchall()[0]
-        if ass_info == []:
-            userinfo = (message.from_user["id"], message.from_user["username"], message.from_user["first_name"], 0, 0)
+        ass_info = cursor.fetchone()
+        await message.answer(ass_info)
+        if ass_info is None:
+            userinfo = (message.from_user["id"], message.from_user["username"], message.from_user["first_name"], 0, 1, 0)
             db.execute(queries["put_user"], userinfo)
             await message.answer("joined")
         else:
             # ass script
-            tmp_length = random.randint(-10,15)
+            if ass_info[4] >= ass_info[5]:
 
-            if tmp_length == 0:
-                # message with no profit
-                pass
-            elif tmp_length > 0:
-                # message with profit
-                pass
-            elif tmp_length < 0:
-                # message with bad profit
-                pass
-            # ass_info[3]
-            ass_info = list(ass_info)
-            ass_info[3] = ass_info[3]+tmp_length
-            userinfo = tuple(ass_info)
+                tmp_length = random.randint(-10, 15)
 
-            await message.answer(userinfo)
+                if tmp_length == 0:
+                    # message with no profit
+                    await message.answer("Размер не поменялся")
+                elif tmp_length > 0:
+                    # message with profit
+                    await message.answer("Увеличилась на {0}".format(tmp_length))
+                elif tmp_length < 0:
+                    # message with bad profit
+                    await message.answer("Уменьшилась на {0}!".format(tmp_length * -1))
+
+                ass_info = list(ass_info)
+                ass_info[3] = ass_info[3]+tmp_length
+
+                if ass_info[3] < 0:
+                    ass_info[3] = 0
+
+                await message.answer("Текущий размер: {0}".format(ass_info[3]))
+
+                # write to database
+
+                timeleft = random.randint(3600, 86400)
+
+                start_time = int(time.time())
+                end_time = time.time() + timeleft
+
+                db.execute("""
+                    UPDATE `users` SET length={0}, starttime={1}, endtime={2} WHERE id={1}
+                """.format(ass_info[3], ass_info[0], start_time, end_time))
+            else:
+                last_time = int(time.time()) - ass_info[5]
+                print(time.localtime(last_time))
+                # await message.answer("Waiting time didn't end, please wait {0} hours and {1} minutes".format())
 
         db.commit()
         db.close()
-        await message.answer("complete")
+        # await message.answer("[+] Operation is completed!")
 
 # help
 @dp.message_handler(commands=["help"])
 async def help(message: types.Message):
     await message.answer(content["help"])
 
+@dp.message_handler(commands=["leave"])
+async def leave(message: types.Message):
+    db = sqlite3.connect("list")
+    db.execute("""
+        DELETE FROM `users` WHERE id={0}
+    """.format(message.from_user["id"]))
+    db.commit()
+    db.close()
+    await message.reply("Вы вышли из игры! Данные о вашей жопке удалены!")
+
 # menu
 @dp.message_handler(commands=["menu"])
 async def menu(message: types.Message):
-    await message.answer("Тут должно быть меню")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    buttons = [
+        types.KeyboardButton(text="/ass"),
+        types.KeyboardButton(text="/leave"),
+        types.KeyboardButton(text="/help")
+    ]
+
+    keyboard.add(*buttons)
+    await message.reply("Меню открыто: ",reply_markup=keyboard)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
